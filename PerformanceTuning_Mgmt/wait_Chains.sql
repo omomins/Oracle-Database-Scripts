142724c585c7cf557f1e79d194458da9f7c4a822
Owais Momin <a-omomin@expediagroup.com>
2022-08-02 12:56:38 +0530

    Update wait_Chains.sql

--- PerformanceTuning_Mgmt/wait_Chains.sql
+++ PerformanceTuning_Mgmt/wait_Chains.sql
@@ -1,39 +1,93 @@
--- Oracle Support Script 
--- Not My script but provides a fast and easy way to troubleshoot database contention and hangs via SQL in version 11g  with v$wait_chains.
+@plusenv
+set linesi 190
+col sidser	format a14  		head 'Sid,Serial'
+col bsid 	format 9999 		head 'Blkd|by'
+col event	format a25 		head 'Wait Event'	trunc
+col w		format a01		head 'x'
+col sqlid	format a13		head 'Sql Id'
+col sp          format a10              head ' Svr - Pgm'
+col orauser     format a08              head 'Oracle|User'      trunc
+col osuser      format a08              head 'OS User'          trunc
+col wobj        format 9999999          head 'Object#'
+col p1          format 9999999999       head 'P1'
+col p2          format 999999           head 'P2'
+col p3          format 99               head 'P3'
+col module	format a26		head 'Module'		trunc
+col sqlid   	format a17		head 'SqlId:Child'
+col elaps	format 999		head 'LCl|Ela'
+col command	format a04		head 'Cmnd'		trunc
 
-set pages 1000
-set lines 120
-set heading off
-column w_proc format a50 tru
-column instance format a20 tru
-column inst format a28 tru
-column wait_event format a50 tru
-column p1 format a16 tru
-column p2 format a16 tru
-column p3 format a15 tru
-column Seconds format a50 tru
-column sincelw format a50 tru
-column blocker_proc format a50 tru
-column waiters format a50 tru
-column chain_signature format a100 wra
-column blocker_chain format a100 wra
-
-
-SELECT *
-FROM (SELECT 'Current Process: '||osid W_PROC, 'SID '||i.instance_name INSTANCE,
-'INST #: '||instance INST,'Blocking Process: '||decode(blocker_osid,null,'<none>',blocker_osid)||
-' from Instance '||blocker_instance BLOCKER_PROC,'Number of waiters: '||num_waiters waiters,
-'Wait Event: ' ||wait_event_text wait_event, 'P1: '||p1 p1, 'P2: '||p2 p2, 'P3: '||p3 p3,
-'Seconds in Wait: '||in_wait_secs Seconds, 'Seconds Since Last Wait: '||time_since_last_wait_secs sincelw,
-'Wait Chain: '||chain_id ||': '||chain_signature chain_signature,'Blocking Wait Chain: '||decode(blocker_chain_id,null,
-'<none>',blocker_chain_id) blocker_chain
-FROM v$wait_chains wc,
-v$instance i
-WHERE wc.instance = i.instance_number (+)
-AND ( num_waiters > 0
-OR ( blocker_osid IS NOT NULL
-AND in_wait_secs > 10 ) )
-ORDER BY chain_id,
-num_waiters DESC)
-WHERE ROWNUM < 101
-/
+prompt === Wait Chains ===
+SELECT 	/*+ rule */
+	 rpad(rpad('+', level ,'-'),4,' ')||lpad(wc.sid,4,' ')||','||lpad(wc.sess_serial#,5,' ') sidser
+       	,wc.blocker_sid		bsid
+	,se.username           	orauser
+	,se.osuser              osuser
+	,lpad(wc.osid,5)||'-'||lpad(substr(nvl(se.program,'null'),instr(se.program,'(')+1,4),4) sp
+	,decode(wc.num_waiters,0,' ','x')	w
+	,wc.wait_event_text	event
+        ,least(se.p1,9999999999)      p1
+        ,least(se.p2,999999)    p2
+        ,least(se.p3,99)        p3
+        ,wc.row_wait_obj#       wobj
+	,nvl(se.module,'<'||substr(se.machine,1,instr(se.machine,'.')-1)||'>')	module
+	,least(se.last_call_et,999)	elaps
+       	,decode (se.command
+	       , 0, ' 0  '
+	       , 1, 'CRTB'
+               , 2, 'ISRT'
+	       , 3, 'SEL'
+	       , 4, 'CRCL'
+	       , 5, 'ALCL'
+	       , 6, 'UPDT'
+	       , 7, 'DEL'
+	       , 8, 'DR'
+	       , 9, 'CRIX'
+	       ,10, 'DRIX'
+	       ,11, 'ALIX'
+	       ,12, 'DRTB'
+	       ,15, 'ALTB'
+	       ,17, 'GRNT'
+	       ,18, 'REVK'
+	       ,19, 'CSYN'
+	       ,20, 'DSYN'
+	       ,21, 'CRVW'
+	       ,22, 'DRVW'
+	       ,26, 'LKTB'
+	       ,27, 'NOOP'
+	       ,28, 'RENM'
+	       ,29, 'CMNT'
+	       ,30, 'AUDT'
+	       ,31, 'NAUD'
+	       ,32, 'CRLN'
+	       ,33, 'DRLN'
+	       ,34, 'CRDB'
+	       ,35, 'ALDB'
+	       ,36, 'CRRB'
+	       ,37, 'ALRB'
+	       ,38, 'DRRB'
+	       ,39, 'CRTS'
+	       ,40, 'ALTS'
+	       ,41, 'DRTS'
+	       ,42, 'ALSE'
+	       ,43, 'ALUS'
+	       ,44, 'COMT'
+	       ,45, 'RLBK'
+	       ,46, 'SVPT'
+	       ,47, 'PLSQ'
+	       ,62, 'ANTB'
+	       ,63, 'ANIX'
+	       ,64, 'ANCL'
+	       ,85, 'TRTB'
+	       ,    to_char(se.command)
+	       )               	command
+	,nvl(se.sql_id,se.prev_sql_id)||':'||decode(se.sql_id,null,'p',se.sql_child_number)		sqlid
+FROM     v$wait_chains 	wc
+	,v$session	se
+where	 wc.sid			= se.sid
+and	 wc.sess_serial#	= se.serial#
+CONNECT BY  PRIOR wc.sid 	= wc.blocker_sid
+AND 	 PRIOR wc.sess_serial# 	= wc.blocker_sess_serial#
+AND 	 PRIOR wc.INSTANCE 	= wc.blocker_instance
+START WITH wc.blocker_is_valid 	= 'FALSE'
+;
